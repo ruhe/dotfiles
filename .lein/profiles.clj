5ec9ae442cc75db706e8a5faaade4028024306f7
Ruslan Kamaldinov <r.kamaldinov@gmail.com>
2016-01-25 15:00:22 +0300

    Fixed leiningen config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,3 @@
-{:user
- {:plugins [[lein-try "0.4.3"]]}}
-
-{:repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+{:user {:plugins [[lein-try "0.4.3"]]}
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
         :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
