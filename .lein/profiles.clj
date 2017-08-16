befc43e3b84971c4fce92f483bb33ac178b01593
Ruslan Kamaldinov <r.kamaldinov@gmail.com>
2017-08-17 01:32:52 +0400

    [lein] updated libraries

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[lein-try "0.4.3"]]}
+{:user {:plugins [[lein-try "0.4.3"]
+                  [lein-kibit "0.1.5"]]}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
         :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
