d65c3b85f2c4fe47e3bf49ffaa6aa6c2a20cd49d
Ruslan Kamaldinov <r.kamaldinov@gmail.com>
2018-10-17 22:37:25 -0700

    Updated lein config

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,3 @@
-{:user {:plugins [[lein-try "0.4.3"]
-                  [lein-kibit "0.1.5"]]}
+{:user {:plugins [[lein-try "0.4.3"]]}
  :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
+        :plugins [[cider/cider-nrepl "0.17.0-SNAPSHOT"]]}}
